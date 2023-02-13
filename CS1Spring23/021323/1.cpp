09a8d67eccb9d9d473dd8625c4c738d9e4cb540f
Corin Chepko <cchepko2@coloradomesa.edu>
2023-02-13 14:44:45 -0700

    stuff

---
+++ CS1Spring23/021323/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int int_var;
+    float float_var = 0;
+    string string_var = "";
+    string string_var2 = "";
+
+    cin >> string_var;
+
+    //getline(cin, string_var2);
+    cout << int_var << endl << float_var << endl << string_var << endl << string_var2 << endl;
+
+    cin.get();
+
+
+    return 0;
+}
---
+++ CS1Spring23/carrots/carrots.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int huff_problems;
+    int contestancts;
+    
+    cin >> contestancts >> huff_problems;
+
+    cout << huff_problems;
+
+    return 0;
+}
